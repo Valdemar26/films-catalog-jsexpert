883a6e3b8e1f554d7a23392fc772fcca6589a97c
Volodymyr Zaplatynskii <volodymyr.zaplatynskii@codelions.co>
2020-10-04 21:11:51 +0300

    18: create module and route for actors

---
+++ src/app/actors/actors.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActorsListComponent } from './components/actors-list/actors-list.component';
+
+
+
+@NgModule({
+  declarations: [ActorsListComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ActorsModule { }
