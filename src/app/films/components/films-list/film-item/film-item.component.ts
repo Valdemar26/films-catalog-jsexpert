90846564b48e20f0d96690c0ffab944e54a46751
v.zaplatunskuj@crosssoft.de <102938qweasd>
2020-10-05 13:25:17 +0300

    20: added 'genres' for films

--- src/app/films/components/films-list/film-item/film-item.component.ts
+++ src/app/films/components/films-list/film-item/film-item.component.ts
@@ -1,31 +1,55 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+
+import { Subscription } from 'rxjs';
 
 import { FilmInterface } from '../../../../film-catalog/interfaces/film.interface';
+import { DataService } from '../../../../services/data.service';
 
 @Component({
   selector: 'exp-film-item',
   templateUrl: './film-item.component.html',
   styleUrls: ['./film-item.component.scss']
 })
-export class FilmItemComponent implements OnInit {
+export class FilmItemComponent implements OnInit, OnDestroy {
 
   @Input() film: FilmInterface;
+  @Input() genresList: any;
 
   @Output() favoriteFilm: EventEmitter<FilmInterface> = new EventEmitter<FilmInterface>();
 
   public isFavoriteFilm: boolean;
   public imagePath: string;
+  public genres = [];
 
-  constructor() { }
+  private subscription: Subscription = new Subscription();
+
+  constructor(private dataService: DataService) { }
 
   public ngOnInit(): void {
-    this.imagePath = 'https://image.tmdb.org/t/p/w500' + this.film.poster_path;
+    this.fetchPosterPath();
+    this.fetchGenres();
+  }
+
+  private fetchPosterPath(): string {
+    return this.imagePath = 'https://image.tmdb.org/t/p/w500' + this.film.poster_path;
   }
 
   public toggleFavoriteFilm(favoriteFilm: FilmInterface): void {
     this.isFavoriteFilm = !this.isFavoriteFilm;
     favoriteFilm.isFavorite = this.isFavoriteFilm;
     this.favoriteFilm.emit(favoriteFilm);
+  }
+
+  public ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  private fetchGenres(): void {
+    this.film.genre_ids.forEach((genre: number) => {
+
+      const item = this.genresList.filter((x) => x.id === genre);
+      this.genres.push(item[0].name);
+    });
   }
 
 }
