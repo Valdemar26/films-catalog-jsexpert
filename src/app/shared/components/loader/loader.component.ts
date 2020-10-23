75208f8740cf77bfc1fd4d9c8f96d89eccc62779
Valdemar26 <z_valdemar@ukr.net>
2020-10-23 12:27:59 +0300

    61:     - try to fix loader

--- src/app/shared/components/loader/loader.component.ts
+++ src/app/shared/components/loader/loader.component.ts
@@ -1,29 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 import { Subscription } from 'rxjs';
 
 import { LoaderService } from '../../services/loader.service';
+
 
 @Component({
   selector: 'exp-loader',
   templateUrl: './loader.component.html',
   styleUrls: ['./loader.component.scss']
 })
-export class LoaderComponent implements OnInit {
+export class LoaderComponent implements OnInit, OnDestroy {
 
   public isLoaderShown: boolean;
+  private subscription: Subscription = new Subscription();
 
   constructor(private loaderService: LoaderService) { }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
     this.checkLoading();
   }
 
-  private checkLoading(): Subscription {
-    return this.loaderService.getLoadingStatus().subscribe((status: boolean) => {
-      console.log(status);  // todo check this!!!!
+  private checkLoading(): void {
+    const loaderSubscription = this.loaderService.getLoadingStatus().subscribe((status: boolean) => {
+      console.log('loader status: ', status);  // todo check this!!!!
       this.isLoaderShown = status;
     });
+
+    this.subscription.add(loaderSubscription);
+  }
+
+  public ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
