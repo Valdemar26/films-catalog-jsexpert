1888f2d3a4edb6ced99140f976b6f2e85f56b2e9
v.zaplatunskuj@crosssoft.de <102938qweasd>
2020-10-28 14:29:50 +0200

    65: - get most popular actors

--- src/app/welcome/services/welcome.service.ts
+++ src/app/welcome/services/welcome.service.ts
@@ -12,12 +12,16 @@ import { environment } from '../../../environments/environment.prod';
 export class WelcomeService {
 
   private apiKey = environment.movieDbApiKey;
-  private themoviedbUrl = `https://api.themoviedb.org/3/movie`;
+  private themoviedbUrl = `https://api.themoviedb.org/3`;
 
   constructor(private http: HttpClient) { }
 
   public getWelcomeFilms(): Observable<any> {
-    return this.http.get(`${this.themoviedbUrl}/popular?page=1&api_key=${this.apiKey}&language=uk-UK`);
+    return this.http.get(`${this.themoviedbUrl}/movie/popular?page=1&api_key=${this.apiKey}&language=uk-UK`);
+  }
+
+  public getWelcomeActors(): Observable<any> {
+    return this.http.get(`${this.themoviedbUrl}/person/popular?page=1&api_key=${this.apiKey}&language=uk-UK`);
   }
 
 
