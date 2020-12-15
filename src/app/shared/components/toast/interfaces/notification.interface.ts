6f2b05ce47b6b82ad3355601dd84fcf97602d40f
Zaplatunskuj <v.zaplatunskuj@crosssoft.de>
2020-12-15 14:04:07 +0200

    128:     - trying to implement subscription to modal confirm button click

--- src/app/shared/components/toast/interfaces/notification.interface.ts
+++ src/app/shared/components/toast/interfaces/notification.interface.ts
@@ -1,9 +1,12 @@
 import { ModalTypeEnum, NotificationTypeEnum } from '../enum/notification-type.enum';
 
+import { Subject } from 'rxjs';
+
 export interface NotificationInterface {
   title: string;
   text: string;
   notificationType?: NotificationTypeEnum.Error;
   modalType?: ModalTypeEnum.Error;
   icon: any;
+  confirm$?: Subject<boolean>;
 }
