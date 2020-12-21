446df6833f1653ba2c3a77c4e35533e4e69dba7b
Volodymyr Zaplatynskii <volodymyr.zaplatynskii@codelions.co>
2020-12-21 23:07:37 +0200

    128:       - fixed bug with crashed sorting (push new film list into stream)

--- src/app/films/pipes/hide-film.pipe.ts
+++ src/app/films/pipes/hide-film.pipe.ts
@@ -1,12 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 import { FilmListInterface } from '../interfaces/film-list.interface';
+import { FilmService } from '../services/film.service';
 
 
 @Pipe({
   name: 'hideFilm'
 })
 export class HideFilmPipe implements PipeTransform {
+
+  constructor(private filmService: FilmService) {}
 
   transform(films: FilmListInterface[]): FilmListInterface[] {
 
@@ -19,7 +22,8 @@ export class HideFilmPipe implements PipeTransform {
         return arrayOfFilteredFilms.indexOf(film.id) === -1;
       });
 
-      localStorage.setItem('filmListArray', JSON.stringify(filteredFilms));  // TODO need to be saved arrays of ids
+      localStorage.setItem('filmListArray', JSON.stringify(filteredFilms));
+      this.filmService.filmList$.next(filteredFilms);
 
       return filteredFilms;
     } else {
