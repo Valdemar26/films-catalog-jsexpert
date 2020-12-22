b0875c91f4b2d8e8c2fec0360554e8b39adb828c
Volodymyr Zaplatynskii <volodymyr.zaplatynskii@codelions.co>
2020-12-22 23:42:47 +0200

    132:       - refactor reverse pipe to use it for release date and comments in the same time

--- src/app/shared/pipe/reverse.pipe.ts
+++ src/app/shared/pipe/reverse.pipe.ts
@@ -6,7 +6,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ReversePipe implements PipeTransform {
 
   transform(comments: any): any {
-    return comments.slice().reverse();
+
+    if (typeof comments === 'string') {
+      return comments.split('-').reverse().join('-');
+    } else if (typeof comments === 'object') {
+      return comments.slice().reverse();
+    }
   }
 
 }
