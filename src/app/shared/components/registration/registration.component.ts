70a671f3d7c3379b72d16758049c32171ab672d2
Volodymyr Zaplatynskii <volodymyr.zaplatynskii@codelions.co>
2021-03-02 21:56:34 +0200

    150:       - remove unused code from login and logout

--- src/app/shared/components/registration/registration.component.ts
+++ src/app/shared/components/registration/registration.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-import { MessagesService } from '../../services/messages.service';
 import { AuthService } from '../../../service/auth.service';
 
 @Component({
@@ -26,8 +25,7 @@ export class RegistrationComponent implements OnInit {
   constructor(
     public authService: AuthService,
     private router: Router,
-    private formBuilder: FormBuilder,
-    private msgService: MessagesService
+    private formBuilder: FormBuilder
   ) { }
 
   public ngOnInit(): void {
@@ -64,33 +62,7 @@ export class RegistrationComponent implements OnInit {
     });
   }
 
-  // registration(): void {
-  //   console.log('REGISTRATION: ', this.registerForm.value.username, this.registerForm.value.login, this.registerForm.value.password);
-  //
-  //   this.editInProgress = false;
-  //   this.authService.login(this.registerForm.value.login, this.registerForm.value.password)
-  //     .subscribe(
-  //       () => {
-  //         this.msgService.setMessage({
-  //           type: 'success',
-  //           body: `${this.registerForm.value.login}, You have successfully logged in to your account. Welcome in Film Catalog!`
-  //         });
-  //         setTimeout(() => {
-  //           this.router.navigate(['/main']);
-  //         }, 2000);
-  //       },
-  //       err => {
-  //         this.msgService.setMessage({
-  //           type: 'danger',
-  //           body: err.error.error
-  //         });
-  //       }
-  //     );
-  // }
-
-
   public goToLogin(): void {
     this.router.navigate(['/login']);
   }
-
 }
