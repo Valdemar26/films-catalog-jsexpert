49359d8cd6135b2189763645aabc840e74fcbad2
Zaplatunskuj <v.zaplatunskuj@crosssoft.de>
2021-03-22 13:59:51 +0200

    155:     - show/hide password directive fixes

--- src/app/shared/directives/input-password/input-password.directive.ts
+++ src/app/shared/directives/input-password/input-password.directive.ts
@@ -1,36 +1,36 @@
-import {ComponentFactoryResolver, ComponentRef, Directive, ElementRef, OnInit, Renderer2, ViewContainerRef} from '@angular/core';
-import {InputPasswordButtonComponent} from './input-password-button/input-password-button.component';
+import { ComponentFactoryResolver, ComponentRef, Directive, ElementRef, OnInit, Renderer2, ViewContainerRef } from '@angular/core';
+
+import { InputPasswordButtonComponent } from './input-password-button/input-password-button.component';
 
 @Directive({
   selector: '[expInputPassword]'
 })
 export class InputPasswordDirective implements OnInit {
 
-  private _buttonComponent: ComponentRef<InputPasswordButtonComponent>;
+  private buttonComponent: ComponentRef<InputPasswordButtonComponent>;
 
   constructor(
-    private _renderer: Renderer2,
-    private _hostElement: ElementRef,
-    private _componentFactoryResolver: ComponentFactoryResolver,
-    private _vcr: ViewContainerRef
+    private renderer: Renderer2,
+    private hostElement: ElementRef,
+    private componentFactoryResolver: ComponentFactoryResolver,
+    private vcr: ViewContainerRef
   ) { }
 
   public ngOnInit(): void {
-    console.log('InputPasswordDirective');
     this.setInputType('Password');
     this.initButtonComponent();
-    this._renderer.setStyle((this._hostElement.nativeElement as HTMLElement).parentNode, 'position', 'relative');
-    this._renderer.setStyle(this._hostElement.nativeElement, 'padding-right', '35px');
+    this.renderer.setStyle((this.hostElement.nativeElement as HTMLElement).parentNode, 'position', 'relative');
+    this.renderer.setStyle(this.hostElement.nativeElement, 'padding-right', '35px');
   }
 
   private initButtonComponent(): void {
-    const factory = this._componentFactoryResolver.resolveComponentFactory(InputPasswordButtonComponent);
-    this._buttonComponent = this._vcr.createComponent(factory);
+    const factory = this.componentFactoryResolver.resolveComponentFactory(InputPasswordButtonComponent);
+    this.buttonComponent = this.vcr.createComponent(factory);
 
-    this._buttonComponent.instance.clickFn = this.setInputType;
+    this.buttonComponent.instance.clickFn = this.setInputType;
   }
 
   private setInputType = (inputType: 'sans-serif' | 'Password') => {
-    this._renderer.setStyle(this._hostElement.nativeElement, 'fontFamily', inputType);
+    this.renderer.setStyle(this.hostElement.nativeElement, 'fontFamily', inputType);
   }
 }
